3283732ca5a7c131920e929bdc1852c07ca177da
VaruneshT <tvarunesharasu@gmail.com>
2025-06-30 19:35:23 +0530

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Intro from "./pages/Intro"
 import Skills from "./pages/Skills"
 import Projects from "./pages/Projects"
 import Education from "./pages/Education"
-import Achievements from "./pages/Achievements"
+// import Achievements from "./pages/Achievements"
 import Contact from "./pages/Contact"
 import LoadingScreen from "./components/loading-screen"
 import ParticleBackground from "./components/particle-background"
@@ -90,7 +90,7 @@ function App() {
                 <Education />
               </div>
               <div ref={achievementsRef}>
-                <Achievements />
+                {/* <Achievements /> */}
               </div>
               <div ref={contactRef}>
                 <Contact />
