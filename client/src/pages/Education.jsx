49512b60e18d318343d62646a31d035bdedb94b7
VaruneshT <tvarunesharasu@gmail.com>
2025-07-22 22:43:41 +0530

    Updated Education.jsx

--- client/src/pages/Education.jsx
+++ client/src/pages/Education.jsx
@@ -125,7 +125,6 @@ export default function Education() {
           </p>
         </motion.div>
 
-        {/* Timeline */}
         <div className="relative">
           {/* Timeline Line */}
           <div className="absolute left-8 top-0 bottom-0 w-1 bg-gradient-to-b from-cyan-400 via-purple-500 to-green-400 rounded-full hidden md:block">
